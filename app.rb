cce3c8e313d480500687d4cecbaf2fb6aca8a399
Matt Sears <matt@mattsears.com>
2011-01-10 22:31:50 -0500

    More style and about page enhancements.

--- app.rb
+++ app.rb
@@ -2,6 +2,29 @@ require 'sinatra'
 
 module Aerial
   class App < Sinatra::Base
+
+    helpers do
+      def page_title(title = nil)
+        @content ||= {}
+        @content[:title] = "Matt Sears"
+        @content[:title] << " | #{title}" if title
+      end
+
+      def body_class(name)
+        @content ||= {}
+        @content[:body_class] = name.to_s
+      end
+
+      def yield_content(key)
+        @content && @content[key]
+      end
+
+      def body_id(name)
+        @content ||= {}
+        @content[:body_id] = name.to_s
+      end
+    end
+
     # Sassy!
     get '/style.css' do
       scss(:style)
